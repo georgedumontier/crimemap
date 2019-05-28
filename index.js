821a51633663aa99493f96c680c1f51772bda897
George DuMontier <gdumonti@meredith.com>
2019-05-28 10:14:40 -0500

    Adds scraper function to account for changing data file urls

--- index.js
+++ index.js
@@ -2,6 +2,36 @@ import axios from "axios";
 import admZip from "adm-zip";
 import csv from "csvtojson";
 import fs from "fs";
+import cheerio from "cheerio";
+
+let thisYear = new Date().getFullYear();
+
+async function getUrl() {
+  try {
+    let webpage = await axios.get(
+      "http://www.atlantapd.org/i-want-to/crime-data-downloads"
+    );
+    // console.log(webpage.data);
+    let html = webpage.data;
+    const $ = cheerio.load(html);
+    let links = $(html).find("a.content_link");
+    let myLink = null;
+    let objectKeys = Object.keys(links);
+    for (let key in objectKeys) {
+      if (
+        $(links[key])
+          .text()
+          .startsWith(`COBRA-${thisYear}`)
+      ) {
+        myLink = links[key];
+        break;
+      }
+    }
+    return `http://www.atlantapd.org${$(myLink).attr("href")}`;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 async function getFile(file) {
   try {
@@ -40,16 +70,15 @@ async function cleanData(jsonArray) {
 
 async function go() {
   try {
-    let zippedFile = await getFile(
-      "http://www.atlantapd.org/Home/ShowDocument?id=2672"
-    );
+    let url = await getUrl();
+    let zippedFile = await getFile(url);
     zippedFile = zippedFile.data;
     let zip = new admZip(zippedFile);
     await zip.extractEntryTo("COBRA-2019.csv", "./rawdata", true, true);
     const jsonArray = await csv().fromFile("./rawdata/COBRA-2019.csv");
     const cleanJSON = await cleanData(jsonArray);
     await fs.writeFile(
-      "./json/COBRA-2019.json",
+      `./json/COBRA-${thisYear}.json`,
       JSON.stringify(cleanJSON),
       err => {
         if (err) {
